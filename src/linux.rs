f1d187b2a8262f065cec0a558202b4654764c480
Csányi István <icsanyi96@gmail.com>
2022-06-10 19:16:02 +0200

    Check chassis type on linux too

--- src/linux.rs
+++ src/linux.rs
@@ -1,6 +1,21 @@
+use std::error::Error;
 use std::path::PathBuf;
 
+const LAPTOP: u16 = 9;
+const NOTEBOOK: u16 = 10;
+
 pub(crate) fn check() -> bool {
-    let lid = PathBuf::from("/proc/acpi/button/lid");
-    lid.exists()
+    // Try to check the chassis type directly
+    if let Ok(laptop) = check_chassis() {
+        return laptop;
+    }
+
+    // If that fails check if the lid exists
+    PathBuf::from("/proc/acpi/button/lid").exists()
+}
+
+fn check_chassis() -> Result<bool, Box<dyn Error>> {
+    let chassis_txt = std::fs::read_to_string("/sys/class/dmi/id/chassis_type")?;
+    let chassis: u16 = chassis_txt.parse()?;
+    Ok(chassis == LAPTOP || chassis == NOTEBOOK)
 }
